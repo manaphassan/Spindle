493985a6fae8b8155a37c36386ed313986b11740
manaphassan <manaphassan@users.noreply.github.com>
2026-09-19 02:26:57 +0800

    build(release): bump version to v1.3.0-alpha (code 4) and app-lite to v1.0.0-alpha (code 2)

--- app-lite/build.gradle.kts
+++ app-lite/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.hana.spindle.lite"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0-LITE"
+        versionCode = 2
+        versionName = "1.0.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.hana.spindle"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.3.0"
+        versionCode = 4
+        versionName = "1.3.0-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
